abac55e84ca61fc0ef0767562ee89d9adcc624e4
sabyanna <bodor.barnabas@cx-ray.com>
2021-02-28 20:58:12 +0100

    login and auth  added

---
+++ app/middlewares/user/auth.js
@@ -0,0 +1,23 @@
+const User = require('../../models/user');
+
+const auth = (req, res, next) => {
+  const { userName } = req.body;
+
+  if (userName === '') {
+    res.status(400).json({ error: 'Missing username' });
+  }
+
+  User.find({ userName })
+    .exec()
+    .then(users => {
+      if (users.length === 0) {
+        res.status(400).json({ error: 'Incorrect username' });
+      }
+      return next();
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+};
+
+module.exports = auth;
--- app/middlewares/user/create.js
+++ app/middlewares/user/create.js
@@ -1,4 +1,3 @@
-
 const User = require('../../models/user');
 
 const create = (req, res, next) => {
